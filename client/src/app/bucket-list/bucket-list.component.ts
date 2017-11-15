5a81388b639897b189443a83be0317b46f5d14dd
aelfwine54 <aelfwine54@gmail.com>
2017-11-15 13:13:45 -0500

    bucket-list

---
+++ client/src/app/bucket-list/bucket-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-bucket-list',
+  templateUrl: './bucket-list.component.html',
+  styleUrls: ['./bucket-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BucketListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
