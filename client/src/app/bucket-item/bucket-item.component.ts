9c33aaa2052ec41eaefd5885dd1ee84b5d220ab4
aelfwine54 <aelfwine54@gmail.com>
2017-11-15 13:13:33 -0500

    bucket-item

---
+++ client/src/app/bucket-item/bucket-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-bucket-item',
+  templateUrl: './bucket-item.component.html',
+  styleUrls: ['./bucket-item.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BucketItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
