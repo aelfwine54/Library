791317668ce097fe6b78fe28ed4ec9dedb25713d
aelfwine54 <aelfwine54@gmail.com>
2017-11-16 14:47:03 -0500

    added cors support

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const bookList = require('./models/books').books;
 const userList = require('./models/users').users;
@@ -13,6 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 // Obtenir la liste de tous les livres
 app.get('/books', function (req, res) {
