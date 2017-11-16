955d97617d922300251c0b89a0fc8f926bbf7452
aelfwine54 <aelfwine54@gmail.com>
2017-11-16 14:47:18 -0500

    added imgUrl to book

--- server/src/models/books.js
+++ server/src/models/books.js
@@ -1,10 +1,11 @@
 class Book {
-  constructor (title, author, year, price) {
+  constructor (title, author, year, price, imgUrl) {
     this.id = Book.incrementId();
     this.title = title;
     this.author = author;
     this.year = year;
     this.price = price;
+    this.imgUrl = imgUrl;
   }
 
   static incrementId () {
@@ -18,10 +19,14 @@ class Book {
 }
 
 const books = [
-  new Book('David Copperfield', 'Charles Dickens', 1850, 19.99),
-  new Book('A study in Scarlet', 'Arthur Conan Doyle', 1887, 15.49),
-  new Book('Le comte de Monte-Cristo', 'Alexandre Dumas', 1845, 34.35),
-  new Book('Mathias Sandorf', 'Jules Verne', 1885, 20.95)
+  new Book('David Copperfield', 'Charles Dickens', 1850, 19.99,
+      'http://3.bp.blogspot.com/-rP86bw2191I/UbvSjZw0okI/AAAAAAAAEoY/ESplku0oZ1s/s1600/copperfield.jpg'),
+  new Book('A study in Scarlet', 'Arthur Conan Doyle', 1887, 15.49,
+      'https://upload.wikimedia.org/wikipedia/commons/2/2c/ArthurConanDoyle_AStudyInScarlet_annual.jpg'),
+  new Book('Le comte de Monte-Cristo', 'Alexandre Dumas', 1845, 34.35,
+      'https://upload.wikimedia.org/wikipedia/commons/d/d6/Louis_Fran%C3%A7ais-Dant%C3%A8s_sur_son_rocher.jpg'),
+  new Book('Mathias Sandorf', 'Jules Verne', 1885, 20.95,
+      'https://images-na.ssl-images-amazon.com/images/I/5194CoDcbqL._SY344_BO1,204,203,200_.jpg')
 ];
 
 module.exports.books = books;
