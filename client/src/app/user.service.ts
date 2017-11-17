3a75a38ae3ed8481c400515b373184a9642188d0
aelfwine54 <aelfwine54@gmail.com>
2017-11-17 13:32:29 -0500

    book-list styling

--- client/src/app/user.service.ts
+++ client/src/app/user.service.ts
@@ -53,8 +53,6 @@ export class UserService {
    */
   private handleError<T> (operation = 'operation', result?: T) {
       return (error: any): Observable<T> => {
-
-          // TODO: send the error to remote logging infrastructure
           console.error(error); // log to console instead
           console.log(operation);
 
